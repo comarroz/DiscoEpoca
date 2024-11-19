52d827baf691bc554cb82da220b53ff5596984f0
ANDERSON ALVES DOS SANTOS <anderson.asantos55@senacsp.edu.br>
2024-11-18 22:11:14 -0300

    Segundo Commit

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
-
-
-
-
-
+     namespace PHP\Modelo;
+     require_once('DAO\CadastrarPessoa.php');
+     require_once('DAO\Conexao.php');
+     use PHP\Modelo\DAO\Conexao;
+ 
 ?>
-
-<!Doctype HTML>
+ 
+<!doctype html>
 <html lang="pt-BR">
-    <head>
+  <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Disco Época</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    </head>
-    <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Disco Época</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,77 +24,61 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
         </li>
+       
        
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Cadastrar
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Cadastar Cliente</a></li>
-            <li><a class="dropdown-item" href="#">Cadastar Funcionário</a></li>
+            <li><a class="dropdown-item" href="DAO\ChamarPessoa.php">Cadastrar Cliente</a></li>
+            <li><a class="dropdown-item" href="DAO\">Cadastrar Funcionario</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Cadastar Vinyl</a></li>
+            <li><a class="dropdown-item" href="#">Cadastrar V</a></li>
           </ul>
         </li>
-
+       
+       
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Consultar
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Consultar Estoque</a></li>
-        
+            <li><a class="dropdown-item" href="ConsultarCodigo.php">Consultar Estoque</a></li>            
           </ul>
-        </li>
-
+        </li>        
+ 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
             Atualizar
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Atualizar Cadastro</a></li>
-           
+            <li><a class="dropdown-item" href="PaginaAtualizar.php">Cadastro</a></li>            
           </ul>
-        </li>
-
+        </li>      
+ 
+ 
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-           Excluir
+            Excluir
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Excluir Cadastro</a></li>
-            
+            <li><a class="dropdown-item" href="#">Excluir Cadastro</a></li>            
           </ul>
-        </li>
-        
+        </li>    
+       
+       
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Pesquise Aqui!</button>
+        <button class="btn btn-outline-success" type="submit">Localizar</button>
       </form>
     </div>
   </div>
 </nav>
-            
-
-         
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ 
+ 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
